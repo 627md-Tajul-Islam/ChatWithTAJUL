40bf3b60f16e0fe25eaac54c9c945d995fe27315
unknown <tajul.azazil@gmail.com>
2021-09-26 17:30:20 +0600

    video

--- public/main.js
+++ public/main.js
@@ -4,4 +4,12 @@ const video = document.querySelector('video')
 
 // get stream
 navigator.mediaDevices.getUserMedia({video:true,audio:true})
-    .then()
+    .then(stream =>{
+        socket.emit('NewClient')
+        video.srcObject = stream
+        video.play()
+
+        function IntiPeer(type){
+            let peer = new Peer({initiator:(type == 'init') ? true : false})
+        }
+    })
