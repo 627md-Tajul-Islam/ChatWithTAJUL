8d65a92447b57b8eb5d9a925fbc5ddec433c44ca
unknown <tajul.azazil@gmail.com>
2021-09-26 22:18:17 +0600

    alhamdulliah

--- public/main.js
+++ public/main.js
@@ -2,7 +2,6 @@ let Peer = require('simple-peer')
 let socket = io()
 const video = document.querySelector('video')
 let client = {}
-
 // get stream
 navigator.mediaDevices.getUserMedia({video:true,audio:true})
 .then(stream => {
